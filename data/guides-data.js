5922763f1fde1aab343df12a0c79d80b2c5dffd1
Juddson Sharp <theJuddson@gmail.com>
2017-03-22 12:18:26 -0400

    Full html5mode false support.

--- data/guides-data.js
+++ data/guides-data.js
@@ -8,56 +8,56 @@ angular
     "name": "Introduction",
     "type": "content",
     "template": "partials/guides.html",
-    "path": "guides",
+    "path": "/#/guides",
     "order": 1
   },
   {
     "name": "Authentication",
     "type": "content",
     "template": "partials/guides/authentication.html",
-    "path": "guides/authentication",
+    "path": "/#/guides/authentication",
     "order": 4
   },
   {
     "name": "Media Query Support",
     "type": "content",
     "template": "partials/guides/breakpoints.html",
-    "path": "guides/breakpoints",
+    "path": "/#/guides/breakpoints",
     "order": 100
   },
   {
     "name": "Configuring your App",
     "type": "content",
     "template": "partials/guides/configuration.html",
-    "path": "guides/configuration",
+    "path": "/#/guides/configuration",
     "order": 3
   },
   {
     "name": "Data API Configuration",
     "type": "content",
     "template": "partials/guides/data-api.html",
-    "path": "guides/data-api",
+    "path": "/#/guides/data-api",
     "order": 4
   },
   {
     "name": "Writing ngBoltJS Documentation",
     "type": "content",
     "template": "partials/guides/documentation.html",
-    "path": "guides/documentation",
+    "path": "/#/guides/documentation",
     "order": 100
   },
   {
     "name": "Getting Started",
     "type": "content",
     "template": "partials/guides/getting-started.html",
-    "path": "guides/getting-started",
+    "path": "/#/guides/getting-started",
     "order": 2
   },
   {
     "name": "Editing Themes",
     "type": "content",
     "template": "partials/guides/theme.html",
-    "path": "guides/theme",
+    "path": "/#/guides/theme",
     "order": 5
   }
 ]);
